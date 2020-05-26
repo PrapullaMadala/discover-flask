4a3322e535be10f97e3ab7eb01a23baa8a86df8c
Prapulla <prapula.m@gmail.com>
2020-05-26 13:31:52 -0400

    data adding to heroku

--- db_create_users.py
+++ db_create_users.py
@@ -1,18 +1,18 @@
 from project import db
 from project.models import User
 
-# insert data
+'''# insert data
 db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
 db.session.add(User("admin", "ad@min.com", "admin"))
-db.session.add(User("mike", "mike@herman.com", "tell"))
+db.session.add(User("mike", "mike@herman.com", "tell"))'''
 
-'''# delete
+# delete
 try:
     num_rows_deleted = db.session.query(User).delete()
     print(num_rows_deleted)
     db.session.commit()
 except:
-    db.session.rollback()'''
+    db.session.rollback()
 
 
 # commit the changes
