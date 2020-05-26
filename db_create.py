b344e8c7d8c51f06b1087e795e26261f948ca1d8
Prapulla <prapula.m@gmail.com>
2020-05-26 13:46:04 -0400

    data adding to heroku

--- db_create.py
+++ db_create.py
@@ -2,13 +2,13 @@ from project import db
 from project.models import BlogPost
 
 # create the database and the  db tables
-# db.create_all()
+db.create_all()
 
-# insert
-db.session.add(BlogPost("Good", "I\'m good.", 5))
-db.session.add(BlogPost("Well", "I\'m well.", 5))
-db.session.add(BlogPost("Flask", "discover_flask_dev", 6))
-db.session.add(BlogPost("postgres", "set up local postgres", 7))
+'''# insert
+db.session.add(BlogPost("Good", "I\'m good."))
+db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("Flask", "discover_flask_dev"))
+db.session.add(BlogPost("postgres", "set up local postgres"))'''
 
 # commit the changes
 db.session.commit()
